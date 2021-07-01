e1ece0bccf21fae0d4d277627587acef2b7292e3
mask-Mac <mask_glb@qq.com>
2021-07-01 22:22:11 +0800

    es-文档组合、范围、模糊、高亮、最大值、分组查询

--- elasticSearch/src/main/java/top/mask/es/doc/ESDocQuery.java
+++ elasticSearch/src/main/java/top/mask/es/doc/ESDocQuery.java
@@ -8,10 +8,17 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
@@ -121,14 +128,172 @@ public class ESDocQuery {
 
 
         //过滤字段
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+//
+//        String[] excludes = {"age"};
+//        String[] includes = {};
+//        builder.fetchSource(includes,excludes);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+        //组合查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+//        boolQueryBuilder.should(QueryBuilders.matchQuery("age",30));
+////        boolQueryBuilder.must(QueryBuilders.matchQuery("granger","男"));
+//        boolQueryBuilder.should(QueryBuilders.matchQuery("age",40));
+//
+//        builder.query(boolQueryBuilder);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+        //范围查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("age");
+//        rangeQuery.gte(30);
+//        rangeQuery.lte(50);
+//
+//        builder.query(rangeQuery);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+        //8.模糊查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        FuzzyQueryBuilder fuzzyQueryBuilder = QueryBuilders.fuzzyQuery("name", "王武").fuzziness(Fuzziness.ONE);
+//
+//
+//        builder.query(fuzzyQueryBuilder);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+
+        //9.高亮查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("age",30);
+//
+//        HighlightBuilder highlightBuilder = new HighlightBuilder();
+//        highlightBuilder.preTags("<font color='red'>");
+//        highlightBuilder.postTags("</font>");
+//        highlightBuilder.field("age");
+//        builder.highlighter(highlightBuilder);
+//        builder.query(termQueryBuilder);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+        //9.聚合查询
+//        SearchRequest request = new SearchRequest();
+//        request.indices("es-doc-batch");
+//
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        MaxAggregationBuilder aggregationBuilder = AggregationBuilders.max("maxAge").field("age");
+//
+//
+//        builder.aggregation(aggregationBuilder);
+//
+//        request.source(builder);
+//        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+//
+//        SearchHits hits = response.getHits();
+//
+//        System.out.println(hits.getTotalHits());
+//        System.out.println(response.getTook());
+//
+//        for (SearchHit hit : hits){
+//            System.out.println(hit.getSourceAsString());
+//        }
+//
+//
+//        esClient.close();
+
+        //9.分组查询
         SearchRequest request = new SearchRequest();
         request.indices("es-doc-batch");
 
-        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("ageGroup").field("age");
 
-        String[] excludes = {"age"};
-        String[] includes = {};
-        builder.fetchSource(includes,excludes);
+
+        builder.aggregation(aggregationBuilder);
 
         request.source(builder);
         SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
